4114ef1ce3729ce61c24b55d9795309be46c530e
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-11-11 16:14:43 +0600

    pagination added in api

--- khoj_app/api/views.py
+++ khoj_app/api/views.py
@@ -1,10 +1,9 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.models import User
-from django.utils.timezone import make_aware
 
 from .serializers import KhojSerializer
 from khoj_app.models import Khoj
@@ -25,13 +24,16 @@ def getKhojes(request):
     user_id = request.GET.get('user_id')
     start_datetime = request.GET.get('start_datetime')
     end_datetime = request.GET.get('end_datetime')
+    paginator = PageNumberPagination()
+    paginator.page_size = 30
+    paginator.max_page_size = 100
     
     # if user is present then perform query
     try:
         user = User.objects.get(pk=user_id)
         khojes = user.khoj_set.filter(created_at__range=[start_datetime, end_datetime])
+        khojes = paginator.paginate_queryset(khojes, request)
         serializer = KhojSerializer(khojes, many=True).data
-        print(serializer)
     except User.DoesNotExist:
         user = None
         serializer = 'User not Found'
